b64ad606436dfc12c9f7c7e932d75a323cb749af
Tiffany <tiffanydong020214@gmail.com>
2024-11-28 16:07:33 -0800

    fix prop error

--- src/app/dashboard/application/university_id/[id]/page.tsx
+++ src/app/dashboard/application/university_id/[id]/page.tsx
@@ -3,9 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import RefereeAddButton from "@/app/ui/referee-add-button";
 
-export default async function UniversityApplicationPage({ params }: {
-    params: { id: string }
-}) {
+export default async function UniversityApplicationPage({ params }: any) {
     const university_id = parseInt(params.id);
 
     const user_id = (await getServerSession(authOptions))?.user.id;
